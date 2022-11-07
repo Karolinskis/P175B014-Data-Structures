05d8f71367c175798500885030ffe3543c6d3fed
Karolis <karolis.paulavicius2002@gmail.com>
2022-11-07 20:11:55 +0200

    Add benchmarks for retainAll method

--- LD2/Lab2_AibesMedziai/src/main/java/edu/ktu/ds/lab2/demo/Benchmark.java
+++ LD2/Lab2_AibesMedziai/src/main/java/edu/ktu/ds/lab2/demo/Benchmark.java
@@ -24,7 +24,12 @@ public class Benchmark {
     public static class FullSet {
 
         Car[] cars;
-        BstSet<Car> carSet;
+        BstSet<Car> carSetBstSearch;
+        BstSet<Car> carSetBst50Percentage;
+        BstSet<Car> carSetBst25Percentage;
+        AvlSet<Car> carSetAvlSearch;
+        AvlSet<Car> carSetAvl50Percentage;
+        AvlSet<Car> carSetAvl25Percentage;
 
         @Setup(Level.Iteration)
         public void generateElements(BenchmarkParams params) {
@@ -33,12 +38,17 @@ public class Benchmark {
 
         @Setup(Level.Invocation)
         public void fillCarSet(BenchmarkParams params) {
-            carSet = new BstSet<>();
-            addElements(cars, carSet);
+            addElements(cars, carSetBstSearch = new BstSet<>(), 100);
+            addElements(cars, carSetBst50Percentage = new BstSet<>(), 50);
+            addElements(cars, carSetBst25Percentage = new BstSet<>(), 25);
+
+            addElements(cars, carSetAvlSearch = new AvlSet<>(), 100);
+            addElements(cars, carSetAvl50Percentage = new AvlSet<>(), 50);
+            addElements(cars, carSetAvl25Percentage = new AvlSet<>(), 25);
         }
     }
 
-    @Param({"10000", "20000", "40000", "80000"})
+    @Param({"5000", "10000", "20000", "40000", "80000"})
     public int elementCount;
 
     Car[] cars;
@@ -52,37 +62,52 @@ public class Benchmark {
         return new CarsGenerator().generateShuffle(count, 1.0);
     }
 
+//    @org.openjdk.jmh.annotations.Benchmark
+//    public BstSet<Car> addBstRecursive() {
+//        BstSet<Car> carSet = new BstSet<>(Car.byPrice);
+//        addElements(cars, carSet);
+//        return carSet;
+//    }
+
+    // retainAll | BST
     @org.openjdk.jmh.annotations.Benchmark
-    public BstSet<Car> addBstRecursive() {
-        BstSet<Car> carSet = new BstSet<>(Car.byPrice);
-        addElements(cars, carSet);
-        return carSet;
+    public void retainAllBst100Percentage(FullSet carSet) {
+        carSet.carSetBstSearch.retainAll(carSet.carSetBstSearch);
     }
 
     @org.openjdk.jmh.annotations.Benchmark
-    public BstSetIterative<Car> addBstIterative() {
-        BstSetIterative<Car> carSet = new BstSetIterative<>(Car.byPrice);
-        addElements(cars, carSet);
-        return carSet;
+    public void retainAllBst50Percentage(FullSet carSet) {
+        carSet.carSetBst50Percentage.retainAll(carSet.carSetBstSearch);
     }
 
     @org.openjdk.jmh.annotations.Benchmark
-    public AvlSet<Car> addAvlRecursive() {
-        AvlSet<Car> carSet = new AvlSet<>(Car.byPrice);
-        addElements(cars, carSet);
-        return carSet;
+    public void retainAllBst25Percentage(FullSet carSet) {
+        carSet.carSetBst25Percentage.retainAll(carSet.carSetBstSearch);
+    }
+
+
+
+    // retainAll | AVL
+    @org.openjdk.jmh.annotations.Benchmark
+    public void retainAllAvl100Percentage(FullSet carSet) {
+        carSet.carSetAvlSearch.retainAll(carSet.carSetAvlSearch);
     }
 
     @org.openjdk.jmh.annotations.Benchmark
-    public void removeBst(FullSet carSet) {
-        for (Car car : carSet.cars) {
-            carSet.carSet.remove(car);
-        }
+    public void retainAllAvl50Percentage(FullSet carSet) {
+        carSet.carSetAvl50Percentage.retainAll(carSet.carSetAvlSearch);
     }
 
-    public static void addElements(Car[] carArray, SortedSet<Car> carSet) {
-        for (Car car : carArray) {
-            carSet.add(car);
+    @org.openjdk.jmh.annotations.Benchmark
+    public void retainAllAvl25Percentage(FullSet carSet) {
+        carSet.carSetAvl25Percentage.retainAll(carSet.carSetAvlSearch);
+    }
+
+
+
+    public static void addElements(Car[] carArray, SortedSet<Car> carSet, double percentage) {
+        for (int i = 0; i < carArray.length * (percentage / 100); i++) {
+            carSet.add(carArray[i]);
         }
     }
 
